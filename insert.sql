477be7180c95539b87d02a099f78b52537cd8e07
AFS0627 <96886305+AFS0627@users.noreply.github.com>
2024-05-07 10:56:53 -0300

    Update insert.sql Arthur

--- insert.sql
+++ insert.sql
@@ -1,1 +1,2 @@
-
+INSERT INTO funcionarios (id_funcionario, tipo_funcionario, login, senha) VALUES (?, ?, ?, ?);
+INSERT INTO produtos (id_produto, produto, tipo_produto, data_chegada, validade_produto, preco) VALUES (?, ?, ?, ?, ?, ?);
