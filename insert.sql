1f6593ce5f302dc6e92fb871ae04baf23efb9d68
CarlosESoares <144946499+CarlosESoares@users.noreply.github.com>
2024-05-07 10:57:50 -0300

    Update insert.sql carlos

--- insert.sql
+++ insert.sql
@@ -1,2 +1,3 @@
 INSERT INTO funcionarios (id_funcionario, tipo_funcionario, login, senha) VALUES (?, ?, ?, ?);
 INSERT INTO produtos (id_produto, produto, tipo_produto, data_chegada, validade_produto, preco) VALUES (?, ?, ?, ?, ?, ?);
+Insert into clientes (id_cliente,nome,sobrenome,cpf_cliente) VALUES(?,?,?,?);
