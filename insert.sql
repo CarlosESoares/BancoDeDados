c54d416c784e501e76a6d169c00d26946da20498
dononi <145597469+dononi@users.noreply.github.com>
2024-05-07 11:09:47 -0300

    Willian Dagnoni Donega

--- insert.sql
+++ insert.sql
@@ -1,3 +1,4 @@
 INSERT INTO funcionarios (id_funcionario, tipo_funcionario, login, senha) VALUES (?, ?, ?, ?);
 INSERT INTO produtos (id_produto, produto, tipo_produto, data_chegada, validade_produto, preco) VALUES (?, ?, ?, ?, ?, ?);
 Insert into clientes (id_cliente,nome,sobrenome,cpf_cliente) VALUES(?,?,?,?);
+insert into carrinho (id_venda,produtos_Id_produto,quantidade) VALUES (?,?,?);
