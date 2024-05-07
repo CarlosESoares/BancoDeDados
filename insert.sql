e884559baced107aba39a848f9335fed9b367551
LucasOliveiraLuz <144946457+LucasOliveiraLuz@users.noreply.github.com>
2024-05-07 11:11:11 -0300

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -2,3 +2,4 @@ INSERT INTO funcionarios (id_funcionario, tipo_funcionario, login, senha) VALUES
 INSERT INTO produtos (id_produto, produto, tipo_produto, data_chegada, validade_produto, preco) VALUES (?, ?, ?, ?, ?, ?);
 Insert into clientes (id_cliente,nome,sobrenome,cpf_cliente) VALUES(?,?,?,?);
 insert into carrinho (id_venda,produtos_Id_produto,quantidade) VALUES (?,?,?);
+INSERT INTO vendas (id_venda,funcionario_id_funcionario,cliente_id_cliente) VALUES (?,?,?);
