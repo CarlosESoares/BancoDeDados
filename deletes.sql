9ad6f58b9f51c3346c293fa6abe847e001908101
LucasOliveiraLuz <144946457+LucasOliveiraLuz@users.noreply.github.com>
2024-05-13 10:11:14 -0300

    Finalizando os deletes

--- deletes.sql
+++ deletes.sql
@@ -3,3 +3,9 @@ DELETE from funcionarios where id_funcionario = ?;
 delete from clientes where id_cliente = ?;
 
 delete from vendas where id_venda = ?;
+
+DELETE FROM credito WHERE id_credito = ?;
+
+DELETE FROM produtos WHERE id_produto = ?;
+
+-- carrinho não tem chave primaria
