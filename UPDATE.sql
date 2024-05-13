764bbe9a6203aa80e3232771830a98682bfebbdd
LucasOliveiraLuz <144946457+LucasOliveiraLuz@users.noreply.github.com>
2024-05-13 10:19:40 -0300

    Update UPDATE.sql

--- UPDATE.sql
+++ UPDATE.sql
@@ -1,1 +1,11 @@
+UPDATE funcionarios SET id_funcionario = 1 WHERE id_funcionario = 0;
 
+UPDATE produtos SET tipo_produto = "Sem estoque" WHERE tipo_produto = "fruta";
+
+UPDATE clientes SET nome = "Carlos" WHERE nome = "Maria";
+
+UPDATE vendas SET id_venda = 24 WHERE id_venda = 2;
+
+UPDATE carrinho quantidade = 2 WHERE quantidade = 7;
+
+UPDATE cretidos limite_divida = 2 WHERE limite_divida = 00;
