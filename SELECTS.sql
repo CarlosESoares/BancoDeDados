1f9509af21382c2cbd9bc49d6bdd410e84e812c7
Arthur <arthur.fr.sc@gmail.com>
2024-05-13 10:37:51 -0300

    fiz

--- SELECTS.sql
+++ SELECTS.sql
@@ -23,3 +23,20 @@ SELECT * FROM carrinho;
 
 -- SELECT com WHERE para login (Exemplo: "funcionarios")
 SELECT * FROM funcionarios WHERE login = ?;
+
+-- SELECT com INNER JOIN/LEFT JOIN/RIGHT JOIN para cada tabela.
+
+select *
+from creditos as c
+ 
+
+inner join vendas as v
+on v.id_venda = c.vendas_id_venda
+
+
+inner join funcionarios as f
+on f.id_funcionario = v.funcionario_id_funcionario
+
+inner join clientes as j
+on j.id_cliente = cliente_id_cliente;
+
