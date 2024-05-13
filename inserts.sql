dfed848a4d346f8ba2d116a7795c9c7ab39fb842
Arthur <arthur.fr.sc@gmail.com>
2024-05-13 10:00:04 -0300

    aa

--- inserts.sql
+++ inserts.sql
@@ -1,4 +1,4 @@
-INSERT INTO funcionarios (id_funcionario, tipo_funcionario, login, senha) VALUES (?, ?, ?, ?);
+-- INSERT INTO funcionarios (id_funcionario, tipo_funcionario, login, senha) VALUES (?, ?, ?, ?);
 
 INSERT INTO funcionarios (tipo_funcionario, login, senha) VALUES ("operador de caixa", "Gustavo06", "01020304");
 
@@ -7,7 +7,7 @@ INSERT INTO funcionarios (tipo_funcionario, login, senha) VALUES ("dono", "Carlo
 INSERT INTO funcionarios (tipo_funcionario, login, senha) VALUES ("estoquista", "Lucas06", "010203");
 -- _______________________________________________________________________________________________________________ 
 
-INSERT INTO produtos (id_produto, produto, tipo_produto, data_chegada, validade_produto, preco) VALUES (?, ?, ?, ?, ?, ?);
+-- INSERT INTO produtos (id_produto, produto, tipo_produto, data_chegada, validade_produto, preco) VALUES (?, ?, ?, ?, ?, ?);
 
 INSERT INTO produtos (produto, tipo_produto, data_chegada, validade_produto, preco) VALUES ("banana", "fruta", "21/03/2024", "21/03/2025", 50.99);
 
@@ -36,6 +36,8 @@ INSERT INTO vendas (funcionario_id_funcionario,cliente_id_cliente) VALUES (2,2);
 INSERT INTO vendas (funcionario_id_funcionario,cliente_id_cliente) VALUES (3,3);
 
 -- __________________________________________________________________________________________________________________
+
+-- INSERT INTO carrinho (id_venda, produtos_Id_produto, quantidade) VALUES (?, ?, ?);
 
 INSERT INTO carrinho (id_venda, produtos_Id_produto, quantidade) VALUES (1, 1, '10');
 
