ef9c73a55d7ab616ff63a5c1c83847a294c349fb
CarlosESoares <144946499+CarlosESoares@users.noreply.github.com>
2024-05-13 07:44:46 -0300

    "Arrumando a ordem"

--- insert.sql
+++ insert.sql
@@ -1,6 +1,11 @@
 INSERT INTO funcionarios (id_funcionario, tipo_funcionario, login, senha) VALUES (?, ?, ?, ?);
+
 INSERT INTO produtos (id_produto, produto, tipo_produto, data_chegada, validade_produto, preco) VALUES (?, ?, ?, ?, ?, ?);
+
 Insert into clientes (id_cliente,nome,sobrenome,cpf_cliente) VALUES(?,?,?,?);
+
 insert into carrinho (id_venda,produtos_Id_produto,quantidade) VALUES (?,?,?);
+
 INSERT INTO vendas (id_venda,funcionario_id_funcionario,cliente_id_cliente) VALUES (?,?,?);
+
 INSERT INTO creditos (id_credito,num_total_parcelas,preco_parcela,vendas_Id_venda,limite_divida,qtd_parcelas_pagas) VALUES (?,?,?,?,?,?);
